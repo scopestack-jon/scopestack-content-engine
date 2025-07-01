0678b3fe909de003d61e15a857922e3142a3443d
scopestack-jon <jon@scopestack.io>
2025-07-01 07:30:23 -0400

    Update Next.js configuration and package dependencies for deployment. Set output to 'export' and configured basePath and assetPrefix for production. Added predeploy and deploy scripts in package.json for streamlined deployment using gh-pages. Updated pnpm-lock.yaml to include new dependencies and ensure compatibility.

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/scopestack-content-engine' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/scopestack-content-engine/' : '',
 }
 
 export default nextConfig
